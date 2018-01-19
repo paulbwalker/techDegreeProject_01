08bfa6b5271541c19ecaa13d0f3e05e4b6f19ec8
Paul Walker <pw97960@gmail.com>
2018-01-19 08:27:45 -0800

    added the setInterval  for 10 sec.

--- js/script.js
+++ js/script.js
@@ -103,6 +103,8 @@ function printQuote() {
 // when user clicks anywhere on the button, the "printQuote" function is called
  document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+ setInterval(printQuote, 10000);
+
 
 
 
