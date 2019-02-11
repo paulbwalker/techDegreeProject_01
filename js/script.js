c8f7e8c8447f7f6be54319d4b11d9894898bc022
Paul Walker <pw97960@gmail.com>
2019-02-11 07:29:48 -0800

    update code

--- js/script.js
+++ js/script.js
@@ -1,113 +1,104 @@
-// Define variable to hold the value of the array in the global scope
-var makeRandomQuotes = 0;
-// Assign quotes to an array of objects
-var quotes = [
-	
-			{ 	quote: 		"Badges? We ain't got no badges! We don't need no badges! I don't have to show you any stinkin' badges!",
-				source: 	"Alfonso Bedoya",
-				citation:   "The Treasure of the Sierra Madre",
-				year: 		1948,
-				tag:        "Movie: Action"   
-			},
-			{ 	quote: 		"All the world's a stage, and all the men and women merely players.",
-				source: 	"William Shakespeare",
-				citation:   "As You Like It",
-				tag:        "Shakespeare"      
-			},
-			{ 	quote: 		"The worst loneliness is not to be comfortable with yourself.",
-				source: 	"Mark Twain",
-				tag:		"Famous Author"  
-			},
-			{ 	quote: 		"Success is not final; failure is not fatal: It is the courage to continue that counts.",
-				source: 	"Winston S. Churchill",
-  				tag:		"Famous Politician"   
-			},
-			{ 	quote: 		"That's what I love most about writers--they're such lousy actors.",
-				source: 	"Vincent H. O'Neil",
-				citation:   "Death Troupe",
-				tag:        "Actors"   
-			},
-			{ 	quote: 		"Life moves pretty fast. You don't stop and look around once in a while, you could miss it.",
-				source: 	"Matthew Broderick",
-				citation:   "Ferris Bueller",
-				year: 		1986,
-				tag:        "Movie: Comedy"   
-			}
+/******************************************
+Treehouse Techdegree:
+FSJS project 1 - A Random Quote Generator
+By Paul B. Walker
+******************************************/
+
+// Got quotes from different places "100 years of movie quotes" was
+// one of the place. "30 Best Movie Comedy quotes" is the other.
+
+const quotes = [
+  {
+    quote: 		"I am serious. And don't call me Shirley.",
+    source: 	"Leslie Nielsen",
+    citation:   "Movie: Airplane",
+    year: 		1980,
+    tag:      "Comedy"
+  },
+  {
+    quote: 		"I'm just one stomach flu away from my goal weight.",
+    source: 	"Emily Blunt",
+    citation:   "Movie: The Deveil Wears Prada",
+    year: 		2006,
+    tag:      "Satire"
+  },
+  {
+    quote: 		"Leave the gun. Take the cannoli.",
+    source: 	"Richard Castellano",
+    citation:   "Movie: The Godfather",
+    year: 		1972,
+    tag:      "Drama"
+  },
+  {
+    quote: 		"It's like I have ESPN or something.",
+    source: 	"Amanda Seyfried",
+    citation:   "Movie: Mean Girls",
+    year: 		2004,
+    tag:      "Comedy"
+  },
+  {
+    quote: 		"Toto, I've a feeling we're not in Kansas anymore.",
+    source: 	"Judy Garland",
+    citation:   "Movie: The Wizard of Oz",
+    year: 		1939,
+    tag:      ""
+  },
+  {
+    quote: 		"All we have to decide is what to do with the time that is given to us.",
+    source: 	"Ian McKellen",
+    citation:   "Movie: The Fellowship of the Ring",
+    year: 		2001,
+    tag:      "SciFi"
+  }
 ];
 
+// Create a function with 2 parameter that takes the low/high numbers for the rgb
+// function
 
-// Anonymous function used to make random quote from the quotes object.
-function getRandomQuote() {
-// Using the Math.random Method to generate the random numbers in the quotes array length 0 to 5.
-	makeRandomQuotes = Math.floor( Math.random() * quotes.length );
-// Assign the global variable and get the number that's generated and the object that's assigned to that number.
-	var makeQuote = quotes[makeRandomQuotes];
-	
-	return makeQuote;	
+const randomNum = (low, high) => {
+  return Math.floor(Math.random() * high) + low;
 }
-// A self contain color changer function when called.
-function bgColor() {
-// Array to contain color names that the browser can recognize.
-	var colors = [ 'maroon','blue', 'orange', 'teal', 'gray', 'purple' ];
-// Get randon color from the array.
-	var getRandomColor = Math.floor( Math.random() * colors.length );
-// Assign a local variable to make color.
-	var makeColor = colors[getRandomColor];
-// Returns make color to body tag of the html.
-	return document.body.style.backgroundColor = makeColor;
-} 
 
-function printQuote() { 
-// Assign local variable that getRandomQuote() function when its called.
-	var printMakeQuote = getRandomQuote();
-// Assign local variable that getRandomColor() function when its called.
-	var bgRandomColor = bgColor();
-// If there is a citation property on the quotes property it will display this variable.
-	var getCitation = isThereCitation(printMakeQuote.citation);
-// If there is a year property with a year, it will display this variable.
-	var getYear = isThereYear(printMakeQuote.year);
+// Calls the randomNum and assign the high/low rgb color value rgb is like the 
+// hexidecemal color value used in CSS limit high number to 175 for darker colors
 
-	var getTags = getTagQuotes(printMakeQuote.tag);
-// Anonymous function to be used if object propery year is in the quote.
-	function isThereYear(objectProp) {	
-		if ( objectProp !== undefined ) {		
-			return '<span class="year">' + objectProp + '</span>';			
-		} else {
-			return '';
-		}		
-	}
-// Anonymous fuction to be used if object propery citation is in the quote.
-	function isThereCitation(objectProp) {	
-		if ( objectProp !== undefined ) {
-			return '<span class="citation">' + objectProp + '</span>';			
-		} else {
-			return '';
-		}		
-	}
-// Anonymous function used if tag propery is in the object. An h2 tag displays in the Div tag with quote-box tag.
-	function getTagQuotes(objectProp) {
-		if ( objectProp !== undefined ) {
-			return '<h2>' + objectProp + '</2>';
-		} else {
-			return '';
-		}
-		
-	}
+const getRandomColor = () => {
+  const rgb = `rgb(
+                    ${randomNum(0, 175)}, 
+                    ${randomNum(0, 175)}, 
+                    ${randomNum(0, 175)}
+                  )`
+  document.body.style.background = rgb;
+}
 
- 
-// Return this code to the .addEventListener when the button is clicked
-	document.getElementById('quote-box').innerHTML = getTags + '<p class="quote">' + printMakeQuote.quote + '</p>'
-    + '<p class="source">' + printMakeQuote.source + getCitation + getYear + '</p>';	
+// I use the lenth property to insure adding more quotes will not effect
+// the program. Also if you add + 1 into the randomNumber you will get
+// an undefined and it comes up randomly.
 
-};
+const getRandomQuote = () => {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
+// Use template literals which made it easier to code
 
-// when user clicks anywhere on the button, the "printQuote" function is called
- document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+const printQuote = () => {
+  let getQuote = getRandomQuote();
+  let displayQuote = `<p class="quote">${getQuote['quote']}</p>
+                      <p class="source">${getQuote['source']}
+                      <span class="citation">${getQuote['citation']}</span>
+                      <span class="year">${getQuote['year']}</span>
+                      <span class="tag">${getQuote['tag']}</span>
+                      </p>`;
+                      
+                      
+  document.getElementById('quote-box').innerHTML = displayQuote;
+  getRandomColor();
+}
 
-// Add setInterval to display different quote and background color every 10 sec.
- setInterval(printQuote, 10000);
+setInterval(printQuote, 30000);
 
+// This line of code came with the project. Didn't change anything to make it work.
 
-
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
